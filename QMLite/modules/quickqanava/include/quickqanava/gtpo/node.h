c3de8b9dab723ebfe1f8695262a94c9e9749d3a3
qinhuajin@creality.com <qhj2025888>
2026-06-29 10:59:55 +0800

    feat:Node功能添加。

--- QMLite/modules/quickqanava/include/quickqanava/gtpo/node.h
+++ QMLite/modules/quickqanava/include/quickqanava/gtpo/node.h
@@ -36,14 +36,50 @@ template<class code_base_t,
     class node_t,
     class edge_t,
     class group_t>
-class node:public code_base_t,
+class node:public node_base_t,
            public graph_property_impl<graph_t>,
            public gtpo::observable_node<node_t,edge_t>
 {
 public:
     friend graph_t;
-    //using nodes_t=
+    using nodes_t=QVector<node_t*>;
+    using observable_base_t=gtpo::observable_node<node_t,edge_t>;
 
+    node(node_base_t* parent=nullptr)noexcept :node_base_t(parent){}
+
+    virtual ~node()noexcept {
+        _int_edges.clear();
+        _out_edges.clear();
+        _in_nodes.clear(false,false);
+        _out_nodes.clear(false,false);
+        if(this->_graph!=nullptr)
+        {
+            std::cerr << "gtpo::node<>::~node(): Warning: Node has been destroyed before beeing removed from the graph." << std::endl;     
+        }
+        this->_graph=nullptr;
+    }
+    node(const node& node){
+        static_cast<void>(node);
+    }
+
+    node &operator=(const node &node)=delete;
+
+    auto add_node_observer(std::unique_ptr<gtpo::node_observer<node_t,edge_t>> observer)noexcept->void{
+        if(observer)
+            observer->set_target(reinterpret_cast<node_t*>(this));
+        observable_base_t::add_node_observer(std::move(observer));
+    }
+
+private:
+    edge_t _int_edges;
+    edge_t _out_edges;
+    nodes_t _in_nodes;
+    nodes_t _out_nodes;
+
+    group_t* _group=nullptr;
+
+    bool  _is_group=false;
+    nodes_t _nodes;
 } // ::gtpo
 
 #include <quickqanava/gtpo/node.hpp>
