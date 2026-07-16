3cb305ac621d20091d2ad69d8d4b82811050eb92
qinhuajin@creality.com <qhj2025888>
2026-07-16 11:45:57 +0800

    feat：完成部分edge.h、node.h

--- QMLite/modules/quickqanava/include/quickqanava/qan/Edge.h
+++ QMLite/modules/quickqanava/include/quickqanava/qan/Edge.h
@@ -2,11 +2,17 @@
 
 #include <QObject>
 #include <quickqanava/gtpo/edge.h>
+#include <QString>
+#include <QPointer>
+#include <QQmlEngine>
+
 
 namespace qan {
 
 class Graph;
 class Node;
+class Group;
+class Nodeitem;
 
 // Phase 2+：qan::Edge 继承 gtpo::edge，把 edge_base_t 设为 QObject
 //
@@ -16,7 +22,7 @@ class Node;
 // - 属性：label, weight, locked, isProtected
 // - 持有 _item 指针（EdgeItem）
 
-class Edge : public QObject
+class Edge : public QObject, public gtpo::edge<QObject, Graph, Node, Group, Nodeitem>
 {
     Q_OBJECT
     // TODO: Phase 2 开始实现
--- QMLite/modules/quickqanava/include/quickqanava/qan/Node.h
+++ QMLite/modules/quickqanava/include/quickqanava/qan/Node.h
@@ -2,12 +2,17 @@
 
 #include <QObject>
 #include <quickqanava/gtpo/node.h>
+#include <QString>
+#include <QPointer>
+#include <QQmlEngine>
+#include <QQuickItem>
 
 namespace qan {
 
 class Graph;
 class Edge;
 class Group;
+class NodeItem;
 
 // Phase 2+：qan::Node 继承 gtpo::node，把 node_base_t 设为 QObject
 //
@@ -18,10 +23,24 @@ class Group;
 // - 持有 _item 指针（NodeItem）
 // - 静态工厂 delegate() / style()
 
-class Node : public QObject
+class Node : public QObject, public gtpo::node<QObject, Graph, Node, Edge, Group>
 {
     Q_OBJECT
-    // TODO: Phase 2 开始实现
+    QML_ELEMENT
+public:
+    using super_t = gtpo::node<QObject, Graph, Node, Edge, Group>;
+    explicit Node(QObject* parent = nullptr);
+    virtual ~Node();
+    Node(const Node& other)=delete;
+
+    Q_PROPERTY(Graph* graph READ getGraph CONSTANT FINAL)
+    const Graph* getGraph() const noexcept;
+    Graph* getGraph() noexcept;
+
+    Q_PROPERTY(NodeItem* item READ getItem CONSTANT)
+    NodeItem* getItem() const noexcept;
+    const NodeItem* getItem() const noexcept;
+    virtual void setItem(NodeItem* item)noexcept;
 };
 
 } // ::qan
